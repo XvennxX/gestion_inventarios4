6bb04de6a7756fd22133c332702198c6f5a4207f
StivenS <svenns109@gmail.com>
2025-05-05 19:37:28 -0500

    creacion diccionario y funciones1

---
+++ main.py
@@ -0,0 +1,20 @@
+from funcion_mostrar import mostrar_inventario
+from funcion_actualizar import actualizar_producto
+
+
+inventario =  {
+    "manzanas" : 50,
+    "naranjas" : 30,
+    "peras" : 20
+}
+
+
+mostrar_inventario(inventario)
+
+actualizar_producto(inventario, 'peras', 25)
+
+mostrar_inventario(inventario)
+
+
+                   
+

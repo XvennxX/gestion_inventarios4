16f9400786385aba51ad1945a36298bb47b52aae
StivenS <svenns109@gmail.com>
2025-05-09 15:31:28 -0500

    Creacion de rama funcion1 y funciones actualizar y mostrar

---
+++ funcion_actualizar.py
@@ -0,0 +1,7 @@
+def actualizar_producto(inventario, producto, nueva_cantidad):
+    if producto in inventario:
+        inventario[producto] = nueva_cantidad
+        print(f"✅ Producto '{producto}' actualizado a {nueva_cantidad} unidades.")
+    else:
+        print(f"⚠️ El producto '{producto}' no existe en el inventario.")
+        
